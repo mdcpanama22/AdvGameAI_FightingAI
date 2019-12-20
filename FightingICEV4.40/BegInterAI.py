9b363c9010772b766c922c96538bb3bfb7c55632
Matthew Carlson <mdcpanama22@gmail.com>
2019-12-20 08:30:53 -0500

    Switched to Python 3.6, and downloaded CUDA for my GPU to finally be able to use Tensorflow on my own machine, now testing the RL algorithm but running to similar bugs in terms of re-running a java environment

--- FightingICEV4.40/BegInterAI.py
+++ FightingICEV4.40/BegInterAI.py
@@ -1,9 +1,24 @@
-# from this import s
-#
-# import gym
-# import gym_fightingice
-# import numpy as np
-#
+import datetime
+import multiprocessing
+import os
+import sys
+
+import gym_fightingice
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import uuid
+
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras import backend as K
+import random
+import tensorflow as tf
+
+K.tensorflow_backend._get_available_gpus()
 # # List of hyper-parameters and constants
 # DECAY_RATE = 0.99
 # BUFFER_SIZE = 40000
@@ -67,26 +82,191 @@
 #         print(self.Q)
 #
 #
-# BrunchAI().testModel()
-import gym
-import gym_fightingice
+# # BrunchAI().testModel()
+# import gym
+# import gym_fightingice
+#
+# def run(env):
+#     rounds = 0
+#     obs, reward, done, info = env.step(env.action_space.sample())
+#     while 1:
+#         if done:
+#             print(reward)
+#             if rounds != 2:
+#                 rounds += 1
+#             else:
+#                 env.reset()
+#                 return
+#         obs, reward, done, info = env.step(env.action_space.sample())
+#
+#
+# env = gym.make("FightingiceDisplayFrameskip-v0",
+#                                     java_env_path="C:/Users/mdcpa/FightingAI/FightingICEV4.40")
+# env.action_space.np_random.seed(123)
+# env.reset()  # Collects Round 1
+# run(env)
 
-def run(env):
-    rounds = 0
-    obs, reward, done, info = env.step(env.action_space.sample())
-    while 1:
-        if done:
-            print(reward)
-            if rounds != 2:
-                rounds += 1
-            else:
-                env.reset()
-                return
-        obs, reward, done, info = env.step(env.action_space.sample())
+# Deep Q-learning Agent
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+              target = reward + self.gamma * \
+                       np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
+class BegInterAI:
+    def __init__(self):
+        # NN variable
+        self.state_size = None
+        self.action_size = None
+        self.AGENT = None
+        self.env = None
+        self.INPUT = 143
+        self.IL = 6  # inner Layer
+        self.OL = 4  # outer layer
+        self.OUTPUT = 56
 
-env = gym.make("FightingiceDisplayFrameskip-v0",
+        self.pop = 8
+        self.mRate = 62 / 100  # Rate for mutations
+        self.pRate = 41 / 100  # Rate for adding a new child from scratch
+        self.PRate = 37 / 100  # Rate for Parent mutation
+        self.generations = 10
+        self.n_indices = 15
+
+        if self.pop <= 0:
+            print("PLEASE CHOOSE A POPULATION GREATER THAN 1")
+            quit()
+
+        self.ID = uuid.uuid1()
+
+    def checkDir(self, Dir):
+        if not (os.path.exists(Dir)):
+            os.mkdir(Dir)
+        else:
+            print("{0} already exists as a directory".format(Dir))
+
+    def DataToCSV(self, name, NN):
+        DataA = pd.DataFrame()
+        DataB = pd.DataFrame()
+        DataC = pd.DataFrame()
+        DataO = pd.DataFrame()
+        A = np.array(NN.weights1)
+        DataA['WeightsA'] = A.flatten()
+        DataB['WeightsB'] = NN.weights2.flatten()
+        DataC['WeightsC'] = NN.weights3.flatten()
+        DataO['output'] = NN.output.flatten()
+
+        time = datetime.now()
+        hour = time.strftime("%H_%M_%S")
+        day = time.strftime("%d")
+        strDir = "BestFighters"
+
+        self.checkDir(strDir)
+        strDir = "{0}/{1}".format(strDir, self.ID)
+        self.checkDir(strDir)
+        strDir = "{0}/{1}".format(strDir, "{0}_{1}_{2}".format(day, name, hour))
+        self.checkDir(strDir)
+
+        DataA.to_csv("{0}/WeightsA_data.csv".format(strDir), index=None, header=True)
+        DataB.to_csv("{0}/WeightsB_data.csv".format(strDir), index=None, header=True)
+        DataC.to_csv("{0}/WeightsC_data.csv".format(strDir), index=None, header=True)
+        DataO.to_csv("{0}/Output_data.csv".format(strDir), index=None, header=True)
+
+    # import sys
+    # import subprocess
+    #
+    # procs = []
+    # for i in range(86):
+    #     proc = subprocess.Popen([sys.executable, 'task.py', '{}in.csv'.format(i), '{}out.csv'.format(i)])
+    #     procs.append(proc)
+    #
+    # for proc in procs:
+    #     proc.wait()
+    # Grab best top half
+    def MainBI(self):
+        self.env = gym.make("FightingiceDisplayFrameskip-v0",
                                     java_env_path="C:/Users/mdcpa/FightingAI/FightingICEV4.40")
-env.action_space.np_random.seed(123)
-env.reset()  # Collects Round 1
-run(env)
+        self.state_size = self.env.observation_space.shape[0]
+        self.action_size = self.env.action_space.n
+        self.AGENT = DQNAgent(self.state_size, self.action_size)
+
+        results = []
+        rounds = 0
+        for i in range(2):
+            state = self.env.reset()
+            state = np.reshape(state, [64, self.state_size])
+            currentC = 0
+            while 1:
+                action = self.AGENT.act(state)
+
+                n_state, reward, done, _ = self.env.step(action)
+                n_state = np.reshape(n_state, [64, self.state_size])
+
+                state = n_state
+
+                if done:
+                    rounds += 1
+                    if rounds == 2:
+                        print("Episode: {}/{}, score: {}".format(i, 10, currentC))
+                        results.append(currentC)
+                        break
+                currentC += 1
+            #self.AGENT.replay(6)
+        plt.plot(results)
+                #self.runOnce()
+
+
+
+def startBAI():
+    BAI = BegInterAI()
+    BAI.MainBI()
+
+
+# if __name__ == '__main__':
+#     import multiprocessing
+#     from multiprocessing import Process
+#     import threading
+#
+#     mp1 = multiprocessing.Process(target=startBAI)
+#     mp2 = multiprocessing.Process(target=startBAI)
+#
+#     mp1.start()
+#     mp2.start()
+if __name__ == '__main__':
+    startBAI()
+    print("worked?")
+    sys.exit()
